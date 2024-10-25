912aa08a0601dda16cb5573ccc857f7cedb36ed6
Samiksha86 <samiksha.lohe6@gmail.com>
2024-10-25 10:29:28 +0530

    Test case with 2 inputs passed

--- src/main/java/com/tdd_git/Calculator.java
+++ src/main/java/com/tdd_git/Calculator.java
@@ -1,10 +1,16 @@
 package com.tdd_git;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Calculator {
 
 	public static int add(String string) {
 		if(!string.isEmpty()) {
-			return Integer.parseInt(string);
+			List<Integer> list = Arrays.stream(string.split(",")).map(Integer:: parseInt)
+					.collect(Collectors.toList());
+			return list.stream().reduce(Integer::sum).orElseThrow();
 		}
 		return 0;
 		
