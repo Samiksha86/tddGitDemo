345345f936b7bb0a52e96d97630ce1d4bd551f63
Samiksha86 <samiksha.lohe6@gmail.com>
2024-10-25 10:14:28 +0530

    Empty string test passed

--- src/main/java/com/tdd_git/Calculator.java
+++ src/main/java/com/tdd_git/Calculator.java
@@ -2,4 +2,9 @@ package com.tdd_git;
 
 public class Calculator {
 
+	public static int add(String string) {
+		
+		return 0;
+		
+	}
 }
--- src/test/java/com/tdd_git/CalculatorTest.java
+++ src/test/java/com/tdd_git/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.tdd_git;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
@@ -7,31 +8,32 @@ public class CalculatorTest {
 
 	@Test
 	void additionZeroForEmptyString() {
+		Assertions.assertThat(Calculator.add("")).isEqualTo(0);
 	}
 	
-	@Test
-	void additionForOneInput() {
-	}
-	
-	@Test
-	void additionForTwoInput() {
-	}
-	
-	@Test
-	void additionForMultipleInput() {
-	}
-	
-	@Test
-	void additionSplitByCommasAndNewLine() {
-	}
-
-	@Test
-	void additionForDelimeter() {
-	}
-	
-	@Test
-	void additionForNegative() {
-	}
+//	@Test
+//	void additionForOneInput() {
+//	}
+//	
+//	@Test
+//	void additionForTwoInput() {
+//	}
+//	
+//	@Test
+//	void additionForMultipleInput() {
+//	}
+//	
+//	@Test
+//	void additionSplitByCommasAndNewLine() {
+//	}
+//
+//	@Test
+//	void additionForDelimeter() {
+//	}
+//	
+//	@Test
+//	void additionForNegative() {
+//	}
 	
 	
 
