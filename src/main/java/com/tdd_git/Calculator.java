3bd1dc03d9d868f1b086fa789c1d47f4a2b6c1e0
Samiksha86 <samiksha.lohe6@gmail.com>
2024-10-25 11:43:09 +0530

    Function created to deal with custom delimeters

--- src/main/java/com/tdd_git/Calculator.java
+++ src/main/java/com/tdd_git/Calculator.java
@@ -6,13 +6,32 @@ import java.util.stream.Collectors;
 
 public class Calculator {
 
+//	public static int add(String string) {
+//		if(!string.isEmpty()) {
+//			List<Integer> list = Arrays.stream(string.split(",|\n")).map(Integer:: parseInt)
+//					.collect(Collectors.toList());
+//			return list.stream().reduce(Integer::sum).orElseThrow();
+//		}
+//		return 0;
+//		
+//	}
+	
 	public static int add(String string) {
 		if(!string.isEmpty()) {
-			List<Integer> list = Arrays.stream(string.split(",|\n")).map(Integer:: parseInt)
-					.collect(Collectors.toList());
+			List<Integer> list = strArrayToIntList(getSplit(string));
 			return list.stream().reduce(Integer::sum).orElseThrow();
 		}
 		return 0;
 		
 	}
+
+   private static List<Integer> strArrayToIntList(Object split) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+   private static Object getSplit(String string) {
+	// TODO Auto-generated method stub
+	return null;
+}
 }
