58a1d92a6fb111fd8c1acdb60779b59554763cef
Samiksha86 <samiksha.lohe6@gmail.com>
2024-10-25 09:47:10 +0530

    git demo

---
+++ src/main/java/com/tdd_git/TddGitApplication.java
@@ -0,0 +1,13 @@
+package com.tdd_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdd_git/TddGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
